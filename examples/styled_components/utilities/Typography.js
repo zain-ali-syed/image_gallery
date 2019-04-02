3f8bc86edd8023c67c71fbe4d977fccc7b72542d
messagetobeats <chelnov63@hotmail.com>
2019-04-02 01:04:38 +0100

    remove redudant folder

--- examples/styled_components/utilities/Typography.js
+++
@@ -1,7 +0,0 @@
-import { css } from 'styled-components';
-
-export const HeaderTypography = css`
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  font-weight: bold;
-  color: white;
-`;
--- examples/styled_components/utilities/index.js
+++
@@ -1,8 +0,0 @@
-import shadow from './Elevation';
-
-// This exports all NAMED exports from Colors (Foe default exports we need to import AND export like normal)
-export * from './Colors';
-export * from './Position';
-export * from './Typography';
-
-export { shadow };
