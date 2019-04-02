275063c6a4d5be1d5871d9e15c857393488ffb48
messagetobeats <chelnov63@hotmail.com>
2019-04-02 01:06:23 +0100

    build folder added

---
+++ build/precache-manifest.8ccc96bcb7cb7e3048b4666745edd5bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "63de06892e150655d284",
+    "url": "/static/js/main.63de0689.chunk.js"
+  },
+  {
+    "revision": "9deef6b9b37414d90e46",
+    "url": "/static/js/2.9deef6b9.chunk.js"
+  },
+  {
+    "revision": "63de06892e150655d284",
+    "url": "/static/css/main.2898e41a.chunk.css"
+  },
+  {
+    "revision": "81ac473313a419b68bb0953317e4a880",
+    "url": "/index.html"
+  }
+];
